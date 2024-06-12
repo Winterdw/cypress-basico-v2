2ff71b025ae5ec3e8faf17dbbb41193eb0195c0b
Daniel Winter <daniel_winter2009@yahoo.com.br>
2024-06-12 11:11:33 -0300

    Section 3 Aula 9

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function(){
         cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type('Daniel')
+        cy.get('#lastName').type("Winter")
+        cy.get('#email').type('daniel_winter2009@yahoo.com.br')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
   })
   
