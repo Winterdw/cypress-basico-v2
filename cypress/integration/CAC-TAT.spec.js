f7c23e316396809f8d9bb7c22f2fb391d8206a8a
Daniel Winter <daniel_winter2009@yahoo.com.br>
2024-06-12 11:48:32 -0300

    Seção 3 Exercício Extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,10 +8,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
     it.only('preenche os campos obrigatórios e envia o formulário', function(){
+        const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
         cy.get('#firstName').type('Daniel')
         cy.get('#lastName').type("Winter")
         cy.get('#email').type('daniel_winter2009@yahoo.com.br')
-        cy.get('#open-text-area').type('Teste')
+        cy.get('#open-text-area').type(longText, {delay: 0})
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
