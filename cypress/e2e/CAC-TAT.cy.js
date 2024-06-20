bc052c91103562d7e6de1f1d905bc87e9786c56c
Daniel Winter <daniel_winter2009@yahoo.com.br>
2024-06-20 10:40:52 -0300

    Aula 6 - Exercício Extra 1

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -91,11 +91,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('input[type="file"]').selectFile('cypress/fixtures/example.json')    
 
     })
-    it.only('seleciona um arquivo simulando um drag-and-drop', function(){
+    it('seleciona um arquivo simulando um drag-and-drop', function(){
         cy.get('input[type="file"]#file-upload').should('not.have.value').selectFile('cypress/fixtures/example.json').should(function($input){
             expect($input[0].files[0].name).to.equal('example.json')
             
         })
-
+    })
+    it.only('seleciona um arquivo simulando um drag-and-drop', function(){
+        cy.get('input[type="file"]#file-upload').should('not.have.value').
+        selectFile('cypress/fixtures/example.json', {action: 'drag-drop'}).
+        should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+            
+        })
     })
   })  
