5c73de24a997c689eef4d806d57780ed0203d0fd
Daniel Winter <daniel_winter2009@yahoo.com.br>
2024-06-20 12:52:17 -0300

    video config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  video:true,
+  videoCompression: true,
   e2e: {
     setupNodeEvents(on, config) {},
   },
